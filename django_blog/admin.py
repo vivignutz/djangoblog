13b775c2ea8f2f42f945971e169c4a18b8926293
vivignutz <vgnutzmann@gmail.com>
2023-01-03 14:24:21 +0000

    Add post search, filter, display, prepolulated fields

--- django_blog/admin.py
+++ django_blog/admin.py
@@ -6,4 +6,9 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    summernote_fields = ('content')
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
