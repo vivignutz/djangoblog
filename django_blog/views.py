c3117debaa12eedd71e55ab0c68975886ee51ca8
vivignutz <vgnutzmann@gmail.com>
2023-01-03 14:57:38 +0000

    Add static pages base, indes and post_detail

--- django_blog/views.py
+++ django_blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class PostList(generic.listView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    templante_name = 'index.html'
+    paginate_by = 6
