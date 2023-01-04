f9ded1cda8adc29bef7d0aace03a7174d0d7b9c8
vivignutz <vgnutzmann@gmail.com>
2023-01-04 09:42:15 +0000

    Settled the view of djangoblog

--- django_blog/views.py
+++ django_blog/views.py
@@ -15,9 +15,9 @@ class PostDetail(View):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by('created_on')
-        like = False
+        liked = False
         if post.likes.filter(id=self.request.user.id).exists():
-            like = True
+            liked = True
 
         return render(
             request,
